215e58d67286e1558e499d86687fc04b14503b5b
Kris Gutmann <astraete.mail@gmail.com>
2022-05-14 00:00:37 +0300

    Fixed footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,7 @@ const Footer = () => {
   return (
     <Fragment>
     <div className="footer">
-      <div className="footer-container">
       <p>Copyright @Kristina Gutmann 2022.  <a href="https://www.flaticon.com/free-stickers/gym" title="gym stickers">Gym stickers created by Stickers - Flaticon</a></p>
-      </div> 
     </div>
     </Fragment>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,9 @@ function Home() {
     <Fragment>
       <Header />
       <Background />
+      <div className='content'>
       <h1>This is the home page if you're logged in and have workouts. See all workouts created.</h1>
+      </div>
       <Footer />
     </Fragment>
   )
