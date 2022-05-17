bc33b32c9f6deb04fa033860a385522d874de26b
Kris Gutmann <astraete.mail@gmail.com>
2022-05-17 10:58:01 +0300

    Header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,83 +1,96 @@
-import { useState, useEffect } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import BiceDB from "../assets/BicepDB.png";
+import BicepDBImage from "../assets/BicepDB.png";
 import "../styles/Header.scss";
 
 function Header() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
+  const handleClick = () => {
+    document.body.classList.toggle("overlay-open");
+    setClick(!click);
   };
 
+  const closeMobileMenu = () => setClick(false);
+
   useEffect(() => {
-    showButton();
+    window.addEventListener("resize", closeMobileMenu);
+
+    return () => {
+      window.removeEventListener("resize", closeMobileMenu);
+    };
   }, []);
 
-  window.addEventListener("resize", showButton);
+  const menuItems = [
+    { name: "My workouts", link: "/" },
+    { name: "Create a workout", link: "/createnew" },
+    { name: "Dashboard", link: "/dashboard" },
+    { name: "Account", link: "/account" },
+    { name: "Workout history", link: "/history" },
+  ];
 
   return (
-    <div className="header">
-      <div className="nav-wrapper">
-        <Link to="/" className="navbar-logo">
-          <img className="logo" src={BiceDB} />
-        </Link>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+    <Fragment>
+      <header className="grid-x align-middle align-justify header">
+        <div className="cell shrink logo">
+          <Link to="/">
+            <img src={BicepDBImage} />
+          </Link>
         </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-              My workouts
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/createnew"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              Create a workout
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/dashboard"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/account" className="nav-links" onClick={closeMobileMenu}>
-              Account
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/history" className="nav-links" onClick={closeMobileMenu}>
-              Workout history
-            </Link>
-          </li>
-          <li>
-            <Link to="/logout" className="nav-links" onClick={closeMobileMenu}>
+
+        <nav className="cell auto show-for-large">
+          <ul className="grid-x align-right">
+            {menuItems.map((menuItem) => {
+              return (
+                <li className="cell shrink" key={menuItem.name}>
+                  <Link to={menuItem.link}>{menuItem.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        <Link
+          className="cell shrink show-for-large button button--x-small button--outline"
+          to="/logout"
+        >
+          Sign out
+        </Link>
+
+        <button
+          className={`cell shrink hide-for-large hamburger hamburger--spin ${
+            click ? "is-active" : ""
+          }`}
+          onClick={handleClick}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
+      </header>
+
+      <div
+        className={`grid-x align-center align-middle hide-for-large mobile-menu ${
+          click ? "active" : ""
+        }`}
+      >
+        <nav className="cell auto">
+          <ul className="grid-x align-center">
+            {menuItems.map((menuItem) => {
+              return (
+                <li className="cell text-center" key={menuItem.name}>
+                  <Link to={menuItem.link}>{menuItem.name}</Link>
+                </li>
+              );
+            })}
+
+            <Link className="cell shrink button button--small" to="/logout">
               Sign out
             </Link>
-          </li>
-        </ul>
-        <div className="menu-close-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "hidden"} />
-        </div>
+          </ul>
+        </nav>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
